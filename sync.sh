d2c5989a9d8693a8f780c3c4d568425a8bda43a3
Mathieu Viossat <mathieu@viossat.fr>
2016-11-17 20:14:30 +0000

    Clean logs

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git pull --all
+git pull --all | grep -Fxv -e 'Already up-to-date.' -e 'Fetching origin' -e 'Fetching upstream'
 for branch in `git branch -r | grep ' *upstream/' | sed 's/^[^\/]*\///g'` ; do git branch --quiet --set-upstream-to=upstream/$branch $branch 2> /dev/null ; done
-git push --all origin
-git push --tags origin
+git push --all origin 2>&1 | grep -Fxv 'Everything up-to-date'
+git push --tags origin 2>&1 | grep -Fxv 'Everything up-to-date'
