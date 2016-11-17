14584f1dc2d3fc4bf0b7ef8199f41799e9619293
Mathieu Viossat <mathieu@viossat.fr>
2016-11-17 20:13:25 +0000

    Fix push

--- sync.sh
+++ sync.sh
@@ -2,5 +2,5 @@
 
 git pull --all
 for branch in `git branch -r | grep ' *upstream/' | sed 's/^[^\/]*\///g'` ; do git branch --quiet --set-upstream-to=upstream/$branch $branch 2> /dev/null ; done
-git push --all
-git push --tags
+git push --all origin
+git push --tags origin
